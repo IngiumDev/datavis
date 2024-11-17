58cb6d0a28064b159bfc43ae2be7e539cf2f92f5
Ingiumdev <86780096+IngiumDev@users.noreply.github.com>
2024-11-17 22:28:46 +0100

    section 2

--- Excercise04.R
+++ Excercise04.R
@@ -5,6 +5,11 @@ library(tidyr)
 library(ggrepel)
 library(hexbin)
 library(patchwork)
+mpg$year <- as.factor(mpg$year)
+ggplot(data = mpg, aes(x=cty, y=hwy, color = year)) + geom_point() + geom_smooth(method='lm')
+mpg
 
+# Section 3
+iris_dt <- as.data.table(iris)
 
-
+ggplot(iris_dt) +geom_histogram()
