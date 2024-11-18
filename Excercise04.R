3253b0e3534c4367f0b7d8897a6dcfb3a6c08a9f
Ingiumdev <86780096+IngiumDev@users.noreply.github.com>
2024-11-18 10:30:03 +0100

    section 2

--- Excercise04.R
+++ Excercise04.R
@@ -11,5 +11,17 @@ mpg
 
 # Section 3
 iris_dt <- as.data.table(iris)
+iris_dt_long <- melt(iris_dt, measure.vars = c("Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width"),variable.name = "Measurement_Type", value.name = "Measurement")
+iris_dt_long
+ggplot(iris_dt_long, aes(Measurement)) + geom_histogram() + facet_grid(~Measurement_Type)
 
-ggplot(iris_dt) +geom_histogram()
+ggplot(iris_dt_long, aes(x=Measurement_Type ,y=Measurement)) + geom_boxplot()  + facet_grid(~Measurement_Type) +geom_jitter() + geom_violin()
+
+ggplot(iris_dt, aes(x=Petal.Length, y=Petal.Width, color=Species)) + geom_point()
+
+x <- rnorm(100)
+x <- as.data.table(x)
+
+ggplot(x, aes(sample=x)) +geom_qq() +geom_abline(slope=1, intercept = 0)
+
+medals_dt <- fread('extdata/lec04_data/lec04_data/exercise/medals.csv')
